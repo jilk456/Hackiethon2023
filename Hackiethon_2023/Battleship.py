dea8ddabe8718eb998f26dcbe7fb7ae5dc97c9b6
jluspencer <journey.luspencer@student.unimelb.edu.au>
2023-09-15 22:00:26 +1000

    restart

--- Hackiethon_2023/Battleship.py
+++ Hackiethon_2023/Battleship.py
@@ -1,27 +1,5 @@
 import random
-def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit):
-
-    def Search(map):
-        map = map
-        print("asdf")
-        
-    def Hunt(lasthit):
-        lasthit = lasthit
-
-    # Generating a map for the enemy
-    def makeEnemyMap():
-        for x in 10:
-            for y in 10:
-                enemyMap[x][y] = 0
-
-    #CHECK IF THERE ARE HITS WITH ADJACET EMPTY SQUARES
-    if 1 == 1:
-        Search("map")
-    else:
-        Hunt("lasthit")
-
+def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
     x = random.randint(1,10)
     y = random.randint(1,10)
-    return [x,y] 
-
-ShipLogic(1,1,1,1,1,1)
+    return [x,y], storage
