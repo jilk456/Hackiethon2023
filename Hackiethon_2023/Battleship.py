9c221c81da85b814e3721f2a6ba48ecaa905e74b
jluspencer <journey.luspencer@student.unimelb.edu.au>
2023-09-15 21:00:27 +1000

    Trial commit 1

--- Hackiethon_2023/Battleship.py
+++ Hackiethon_2023/Battleship.py
@@ -3,3 +3,10 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit):
     x = random.randint(1,10)
     y = random.randint(1,10)
     return [x,y] 
+    
+    # Generating a map for the enemy
+    def makeEnemyMap():
+        for x in 10:
+            for y in 10:
+                enemyMap[x][y] = 0
+    
