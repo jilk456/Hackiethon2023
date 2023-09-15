5cedae31cbdb42fda4ad9defa7eac58de4d7e955
jluspencer <journey.luspencer@student.unimelb.edu.au>
2023-09-15 22:37:29 +1000

    Commit after Josh's hunt algorithm

--- Hackiethon_2023/Battleship.py
+++ Hackiethon_2023/Battleship.py
@@ -1,5 +1,45 @@
 import random
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
-    x = random.randint(1,10)
-    y = random.randint(1,10)
-    return [x,y], storage
+
+    #Position refers to the coordinates of the top left
+    def checkIfFits(shiplength, direction, positionX, positionY):
+        if direction == "alongX":
+            requiredPositions = [[x+positionX,positionY] for x in range(shiplength)]
+        else:
+            requiredPositions = [[positionX,positionY+y] for y in range(shiplength)]
+        for position in requiredPositions:
+            if not((position[0] < 10) and (position[1] < 10) and (map[position[0]][position[1]]!=1)):
+                return False
+        return True
+    
+    # Takes [x, y] list as input and gives the number of ship positions that could be on it
+    def makeProbabilityGrid():
+        ships = [5, 3, 3, 2, 2]
+        probabilityGrid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+                           [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]]
+        
+    
+    #Generates a map if there isn't one yet. 1 = hit, -1 = miss, 0 = unchecked
+    if not storage:
+        map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]]
+    else:
+        map = storage
+    
+    return [x,y], map
