0f9a6987d8fc9ee3dffbd3436ac9c7b20767d72e
Josh <jrlplano@gmail.com>
2023-09-15 21:02:32 +1000

    test change #1

--- Hackiethon_2023/Battleship.py
+++ Hackiethon_2023/Battleship.py
@@ -1,5 +1,22 @@
 import random
+from re import search
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit):
+
+    def Search(map):
+        map = map
+        print("asdf")
+        
+    def Hunt(lasthit):
+        lasthit = lasthit
+
+    #CHECK IF THERE ARE HITS WITH ADJACET EMPTY SQUARES
+    if 1 == 1:
+        Search("map")
+    else:
+        Hunt("lasthit")
+
     x = random.randint(1,10)
     y = random.randint(1,10)
     return [x,y] 
+
+ShipLogic(1,1,1,1,1,1)
