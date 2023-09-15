6d79349a16b98e03ecfed3e5b0e453995abdf249
Josh <jrlplano@gmail.com>
2023-09-15 22:48:20 +1000

    Merge Cleanup

--- Hackiethon_2023/Battleship.py
+++ Hackiethon_2023/Battleship.py
@@ -29,103 +29,7 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
             else:
                 Search()
 
-    x = 5
-    y = 5
-    
-    #CHOOSE WHAT ALGO
-    if p1PrevHit == True:
-        #CHECK IF LAST HIT HAS EMPTY ADJACENT SQUARES
-        x = p1ShotSeq[-1][0]
-        y = p1ShotSeq[-1][1]
-        for i in range(4):
-            if i == 0:
-                if (x + 1) < 10 and storage[x + 1][y] == 0:
-                    Hunt()
-                    break 
-            elif i == 1:
-                if (x - 1) > -1 and storage[x - 1][y] == 0:
-                    Hunt()
-                    break 
-            elif i == 2:
-                if (y + 1) < 10 and storage[x][y + 1] == 0:
-                    Hunt()
-                    break 
-            elif i == 3:
-                if (y - 1) > -1 and storage[x][y - 1] == 0:
-                    Hunt()
-                    break 
-            else:
-                Search()
-    else:
-        Search()
-        
-
-
-
-    def Search():
-        print("searching")
-        
-    def Hunt():
-        print("hunting")
-        x = p1ShotSeq[-1][0]
-        y = p1ShotSeq[-1][1]
-        print(str([x,y]) + " this is the hunt call")
-        for i in range(4):
-            if i == 0:
-                if (x + 1) < 10 and storage[x + 1][y] == 0:
-                    x += 1
-                    break 
-            elif i == 1:
-                if (x - 1) > -1 and storage[x - 1][y] == 0:
-                    x -= 1
-                    break 
-            elif i == 2:
-                if (y + 1) < 10 and storage[x][y + 1] == 0:
-                    y += 1
-                    break 
-            elif i == 3:
-                if (y - 1) > -1 and storage[x][y - 1] == 0:
-                    y -= 1
-                    break 
-            else:
-                Search()
-
-    x = 5
-    y = 5
-    
-    #CHOOSE WHAT ALGO
-    if p1PrevHit == True:
-        #CHECK IF LAST HIT HAS EMPTY ADJACENT SQUARES
-        x = p1ShotSeq[-1][0]
-        y = p1ShotSeq[-1][1]
-        for i in range(4):
-            if i == 0:
-                if (x + 1) < 10 and storage[x + 1][y] == 0:
-                    Hunt()
-                    break 
-            elif i == 1:
-                if (x - 1) > -1 and storage[x - 1][y] == 0:
-                    Hunt()
-                    break 
-            elif i == 2:
-                if (y + 1) < 10 and storage[x][y + 1] == 0:
-                    Hunt()
-                    break 
-            elif i == 3:
-                if (y - 1) > -1 and storage[x][y - 1] == 0:
-                    Hunt()
-                    break 
-            else:
-                Search()
-    else:
-        Search()
-        
-
-
-
-    return [x,y], storage
-
-    #Position refers to the coordinates of the top left
+#Position refers to the coordinates of the top left
     def checkIfFits(shiplength, direction, positionX, positionY):
         if direction == "alongX":
             requiredPositions = [[x+positionX,positionY] for x in range(shiplength)]
@@ -149,9 +53,8 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]]
-        
     
-    #Generates a map if there isn't one yet. 1 = hit, -1 = miss, 0 = unchecked
+        #Generates a map if there isn't one yet. 1 = hit, -1 = miss, 0 = unchecked
     if not storage:
         map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
@@ -165,7 +68,40 @@ def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]]
     else:
         map = storage
+
+
+
+    x = 5
+    y = 5
     
+    #CHOOSE WHAT ALGO
+    if p1PrevHit == True:
+        #CHECK IF LAST HIT HAS EMPTY ADJACENT SQUARES
+        x = p1ShotSeq[-1][0]
+        y = p1ShotSeq[-1][1]
+        for i in range(4):
+            if i == 0:
+                if (x + 1) < 10 and storage[x + 1][y] == 0:
+                    Hunt()
+                    break 
+            elif i == 1:
+                if (x - 1) > -1 and storage[x - 1][y] == 0:
+                    Hunt()
+                    break 
+            elif i == 2:
+                if (y + 1) < 10 and storage[x][y + 1] == 0:
+                    Hunt()
+                    break 
+            elif i == 3:
+                if (y - 1) > -1 and storage[x][y - 1] == 0:
+                    Hunt()
+                    break 
+            else:
+                Search()
+    else:
+        Search()
+
+
     return [x,y], map
 
 
